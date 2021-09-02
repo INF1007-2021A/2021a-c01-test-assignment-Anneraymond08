a569e5424daa94d27d882c8d99ac99b2ba2a303a
Anneraymond08 <90001135+Anneraymond08@users.noreply.github.com>
2021-09-02 15:06:30 -0400

    exercice.py
    test 2

--- exercice.py
+++ exercice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import end as end
 
 if __name__ == '__main__':
     #TODO: Modifier le code ici
     print("Le cours INF1007 est mon cours préféré!")
-
